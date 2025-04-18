2a9d21155b1e7cb0c225883fb1a328a8b1036e67
occemper <ravilre22@gmail.com>
2025-04-18 15:04:48 +0300

    005 Seeding Data for the REST API

---
+++ database/seeders/EventSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Event;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class EventSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = User::all();
+
+        for ($i = 0; $i < 200; $i++) {
+            $user = $users->random();
+            Event::factory()->create([
+                "user_id" => $user->id
+            ]);
+        }
+    }
+}
