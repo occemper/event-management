3c8a9aade9c7987969d749acdb004041e8fe7151
occemper <ravilre22@gmail.com>
2025-04-29 23:49:08 +0300

    017 Revoking Tokens and Signing Out

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,16 @@ use App\Http\Controllers\Api\EventController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::middleware('auth:sanctum')
+    ->get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->middleware('auth:sanctum');
+
+
 // Public routes
 Route::apiResource('events', EventController::class)
     ->only(['index', 'show']);
