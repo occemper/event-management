1e35e302ba82647c729abc5b2a1f36ee5d110ff6
occemper <ravilre22@gmail.com>
2025-05-04 20:49:53 +0300

    022 Reminding About Events - Custom Artisan Command

---
+++ app/Console/Commands/SentEventReminders.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use illuminate\Support\Str;
+
+class SentEventReminders extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:sent-event-reminders';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Sends notifications to all event attendees thst event starts soon';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $events = \App\Models\Event::with('attendees.user')
+            ->whereBetween('start_time', [now(), now()->addDay()])
+            ->get();
+
+        $eventCount = $events->count();
+        $eventLabel = Str::plural('event', $eventCount);
+
+        $events->each(
+            fn($event) => $event->attendees()->each(
+                fn($attendee) => $this->info("Notifying the user {$attendee->user->id}")
+            )
+        );
+
+        $this->info("Found {$eventCount} {$eventLabel}");
+    }
+}
